5358ed7d521c3a9516e5be2c1997a702278b84d1
Mehmet Kamil Torunoğlu <98940977+torunoglukamil@users.noreply.github.com>
2024-05-03 15:38:41 +0300

    Public Transport Demo

---
+++ publicTransportDemo/src/main/java/com/example/public_transport/PublicTransportDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.public_transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicTransportDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicTransportDemoApplication.class, args);
+	}
+
+}
---
+++ publicTransportDemo/src/test/java/com/example/public_transport/PublicTransportDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.public_transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicTransportDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
