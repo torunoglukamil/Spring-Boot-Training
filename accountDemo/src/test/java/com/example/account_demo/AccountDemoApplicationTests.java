ada5daadb9f14abb88b5a7a89944e84f20777653
Mehmet Kamil Torunoğlu <98940977+torunoglukamil@users.noreply.github.com>
2024-05-09 07:48:31 +0300

    Account Demo

---
+++ accountDemo/src/test/java/com/example/account_demo/AccountDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.account_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
